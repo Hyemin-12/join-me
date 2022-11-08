efa78f8dc4d748fdfec438c3125ad847583a9d80
Hyemin-12 <w2112@e-mirim.hs.kr>
2022-11-08 22:53:39 +0900

    project editRole 추가 및 팝업 띄우기

--- project/js/project.js
+++ project/js/project.js
@@ -1,21 +1,35 @@
 window.onload = () => {
+    // role 팝업
     let openRole = document.getElementById('role-open');
-    if(openRole){
-        openRole.onclick = openRolePopup;
-    }
+    if(openRole) openRole.onclick = openRolePopup;
     let closeRole = document.getElementById('role-close');
-    if(closeRole){
-        closeRole.onclick = closeRolePopup;
-    }
+    if(closeRole) closeRole.onclick = closeRolePopup;
+
+    // editRole 팝업
+    let openEditRole = document.getElementById('edit-role-open');
+    if(openEditRole) openEditRole.onclick = openEditRolePopup;
+    let closeEditRole = document.getElementById('edit-role-close');
+    if(closeEditRole) closeEditRole.onclick = closeEditRolePopup;
 }
 
+// role 팝업
 // 팝업 열기
 const openRolePopup = () => {
     document.getElementById('role-page').style.display = 'block';
 }
-
-
 // 팝업 닫기
 const closeRolePopup = () => {
     document.getElementById('role-page').style.display = 'none';
+}
+
+// editRole 팝업
+// 팝업 열기
+const openEditRolePopup = () => {
+    document.getElementById('edit-role-page').style.display = 'block';
+    document.getElementById('role-page').style.display = 'none';
+}
+// 팝업 닫기
+const closeEditRolePopup = () => {
+    document.getElementById('edit-role-page').style.display = 'none';
+    document.getElementById('role-page').style.display = 'block';
 }
