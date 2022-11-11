5aa77e7a3bfc1af026127e08e2186c77b8a05ac5
Hyemin-12 <w2112@e-mirim.hs.kr>
2022-11-12 00:41:25 +0900

    app.js, project 연결

--- project/project.js
+++ project/project.js
@@ -6,4 +6,9 @@ router.get('/create', (req, res) => {
     res.sendFile(__dirname + '/createProject.html');
   })
 
+// 프로젝트 페이지
+router.get('/', (req, res) => {
+  res.sendFile(__dirname + '/project.html');
+})
+
 module.exports = router;
