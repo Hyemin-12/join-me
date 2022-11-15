8fe04e379c521189cc26964665dfae2813e21cdf
Hyemin-12 <w2112@e-mirim.hs.kr>
2022-11-15 22:18:07 +0900

    createProject router.post

--- project/project.js
+++ project/project.js
@@ -4,7 +4,16 @@ var router = express.Router();
 // 프로젝트 생성 페이지
 router.get('/create', (req, res) => {
     res.sendFile(__dirname + '/createProject.html');
-  })
+})
+
+router.post('/create_progress', function (request, response) {
+    let title = request.body.title;
+    let manager = request.body.manager;
+    let date = request.body.datepicker;
+    let info = request.body.info;
+
+    console.log(title, manager, date, info)
+})
 
 // 프로젝트 페이지
 router.get('/', (req, res) => {
