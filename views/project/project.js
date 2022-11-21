efa2224465d768a1702263fdc285ea717f381b5f
Hyemin-12 <w2112@e-mirim.hs.kr>
2022-11-21 19:52:46 +0900

    입력된 정보 없을 시 alert 창 띄우기

--- views/project/project.js
+++ views/project/project.js
@@ -19,11 +19,16 @@ router.post('/', (req, res) => {
     let value = req.body.value;
     let info = req.body.info;
 
-    db.query('INSERT INTO projectTable (title, date, value, members_name) VALUES (?, ?, ?, ?)', [title, date, value, info], function (err, result) {
-      if (err) throw err;
-      else res.send(`<script type="text/javascript">alert("프로젝트 생성 완료!");
-                    document.location.href="/";</script>`);
-    });
+    if(title && date){
+      db.query('INSERT INTO projectTable (title, date, value, members_name) VALUES (?, ?, ?, ?)', [title, date, value, info], function (err, result) {
+        if (err) throw err;
+        else res.send(`<script type="text/javascript">alert("프로젝트 생성 완료!");
+                    	document.location.href="/";</script>`);
+      });
+    } else {
+		res.send(`<script type="text/javascript">alert("입력하지 않은 정보가 있습니다!");
+                    	document.location.href="/";</script>`);
+	}
 })
 
 module.exports = router;
