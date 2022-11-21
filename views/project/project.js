e9b2ce8be32f00b3c8dab6f66049fcd07dad2e93
Hyemin-12 <w2112@e-mirim.hs.kr>
2022-11-21 19:37:52 +0900

    db에 project 정보 저장하기

--- views/project/project.js
+++ views/project/project.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../lib_login/db');
+
 // 프로젝트 생성 페이지
 router.get('/create', (req, res) => {
     res.sendFile(__dirname + '/createProject.html');
@@ -17,7 +19,11 @@ router.post('/', (req, res) => {
     let value = req.body.value;
     let info = req.body.info;
 
-    res.render('../views/lib_login/index.html', {data : [title, date, value, info]});
+    db.query('INSERT INTO projectTable (title, date, value, members_name) VALUES (?, ?, ?, ?)', [title, date, value, info], function (err, result) {
+      if (err) throw err;
+      else res.send(`<script type="text/javascript">alert("프로젝트 생성 완료!");
+                    document.location.href="/";</script>`);
+    });
 })
 
 module.exports = router;
