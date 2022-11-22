9417c11caf8ce196d7c70e16ac3c49f34e32d552
Hyemin-12 <w2112@e-mirim.hs.kr>
2022-11-23 07:39:05 +0900

    .

--- project/project.js
+++ project/project.js
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
   res.sendFile(__dirname + '/project.html');
 })
 
-router.post('/', (req, res) => {
+router.post('/create', (req, res) => {
     let title = req.body.title;
     let date = req.body.datepicker;
     let value = req.body.value;
@@ -22,7 +22,7 @@ router.post('/', (req, res) => {
     if(title && date){
       db.query('INSERT INTO projectTable (title, date, value, members_name) VALUES (?, ?, ?, ?)', [title, date, value, info], function (err, result) {
         if (err) throw err;
-        else res.send(`<script type="text/javascript">alert("프로젝트 생성 완료!");
+        else res.status(200).send(`<script type="text/javascript">alert("프로젝트 생성 완료!");
                     	document.location.href="/";</script>`);
       });
     } else {
